c3820944378eb186f4341de757896cec88b11054
Gregor Karetka <karetka.gregor@gmail.com>
2022-04-03 21:47:13 +0200

    Update main_remo.py

--- py_scripts/leet_codes/remo_substr_in_str/main_remo.py
+++ py_scripts/leet_codes/remo_substr_in_str/main_remo.py
@@ -1,21 +1,26 @@
-s = "uihoudaaaabcbcbcbcdwabacbccuioinababcch"
+s = "daabcbaabcbc"
 subs = "abc"
 
-i = 0
-while i < len(s):
-    if s[i:i+len(subs)] == subs:
-        s = s[0:i] + s[i+len(subs):-1]
-        i = i - len(subs)
-    else:
+def perform(_s: str, _subs: str):
+    stck = ""
+
+    i = 0
+    while i < len(_s):
+        stck += _s[i]
+        while stck[len(stck)-len(_subs):len(stck)] == _subs:
+            stck = stck.removesuffix(_subs)
         i += 1
+
+    print(stck)
+
 
 
 def test_it(_s: str, _substr: str):
-    while s.count(_substr) > 0:
+    while _s.count(_substr) > 0:
         _s = _s.replace(_substr, "")
 
     print(_s)
 
 
+perform(s, subs)
 test_it(s, subs)
-print(s)
